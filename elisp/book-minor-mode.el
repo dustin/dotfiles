86c048afac49188ca4aa360b604431313686644f
Dustin Sallings <dustin@spy.net>
2018-03-09 22:56:40 -0800

    Some book-mode updates.

--- elisp/book-minor-mode.el
+++ elisp/book-minor-mode.el
@@ -1,13 +1,22 @@
-(defun book-wrap (b e)
+(defun book-wrap (b e &optional each)
   (save-excursion
     (save-restriction
       (narrow-to-region (region-beginning) (region-end))
       (replace-string "<" "&lt;" nil (point-min) (point-max))
       (replace-string ">" "&gt;" nil (point-min) (point-max))
+      (if each
+          (progn
+            (goto-char (point-min))
+            (while (< (point) (point-max))
+              (funcall each)
+              (forward-line))))
       (goto-char (point-max))
       (insert e)
       (goto-char (point-min))
       (insert b))))
+
+(defun kill-prefix (c)
+  (while (equal (char-after) c) (delete-char 1)))
 
 (defun book-email-header ()
   (interactive)
@@ -17,6 +26,7 @@
     (dotimes (number 4 value)
       (beginning-of-line)
       (save-excursion
+        (delete-horizontal-space)
         (replace-string "<" "&lt;" nil (line-beginning-position) (line-end-position))
         (replace-string ">" "&gt;" nil (line-beginning-position) (line-end-position)))
       (insert "<p>")
@@ -29,17 +39,35 @@
 
 (defun book-quote ()
   (interactive)
-  (book-wrap "<blockquote>\n<p>\n" "</p>\n</blockquote>\n"))
+  (book-wrap "<blockquote>\n<p>\n" "</p>\n</blockquote>\n"
+             (lambda ()
+               (kill-prefix ?#)
+               (delete-horizontal-space)))
+  (forward-line 2))
+
 
 (defun book-p ()
   (interactive)
-  (book-wrap "<p>\n" "</p>\n"))
+  (book-wrap "<p>\n" "</p>\n")
+  (forward-line))
+
+(defun book-paste-email ()
+  (interactive)
+  (save-excursion
+    (let ((p (point)))
+      (yank)
+      (insert "\n\n<hr/>\n")
+      (replace-string " " " " nil p (point))
+      (goto-char p)
+      (book-email-header)))
+  (forward-line 6))
 
 (defvar book-mode-keymap
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-c h") 'book-email-header)
     (define-key map (kbd "C-c p") 'book-p)
     (define-key map (kbd "C-c q") 'book-quote)
+    (define-key map (kbd "C-c y") 'book-paste-email)
     map)
    "Key mapping for book mode")
 
