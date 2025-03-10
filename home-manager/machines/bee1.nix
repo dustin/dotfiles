159f40061a52b304f2aaffb8ec85daaaf5936c3c
Dustin Sallings <dustin@spy.net>
2025-03-09 20:14:06 -1000

    Need my nut-to-mqtt running

--- home-manager/machines/bee1.nix
+++ home-manager/machines/bee1.nix
@@ -51,6 +51,22 @@ in
         };
       };
 
+      nuttomqtt = {
+        Install = { WantedBy = ["default.target"]; };
+
+        Unit = {
+          Description = "nut to mqtt";
+          After = "network.target";
+        };
+
+        Service = {
+          ExecStart = ''/home/dustin/.local/bin/nut-to-mqtt -mqtt_clientid="" -mqtt_endpoint=tcp://mqtt:1883/ -nut_username=upsmon -nut_password=somepassword'';
+          Restart = ''always'';
+          StartLimitInterval = 0;
+          RestartSec = 60;
+        };
+      };
+
       gitmirror = {
         Install = { WantedBy = ["default.target"]; };
 
--- home-manager/machines/bee2.nix
+++ home-manager/machines/bee2.nix
@@ -27,6 +27,23 @@
           TimeoutStartSec = 300;
         };
       };
+
+      nuttomqtt = {
+        Install = { WantedBy = ["default.target"]; };
+
+        Unit = {
+          Description = "nut to mqtt";
+          After = "network.target";
+        };
+
+        Service = {
+          ExecStart = ''/home/dustin/.local/bin/nut-to-mqtt -mqtt_clientid="" -mqtt_endpoint=tcp://mqtt:1883/ -nut_username=upsmon -nut_password=somepassword'';
+          Restart = ''always'';
+          StartLimitInterval = 0;
+          RestartSec = 60;
+        };
+      };
+
     };
   };
 
