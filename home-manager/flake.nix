6547758d2760738f15a9ae5190fa242f50f98cdd
Dustin Sallings <dustin@spy.net>
2026-07-08 08:36:03 -1000

    Use a newer duckdb

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -1,6 +1,5 @@
 {
   description = "Dustin's Home Manager configuration";
-
   inputs = {
     nixpkgs-old.url = "github:NixOS/nixpkgs/c53baa6685261e5253a1c355a1b322f82674a824";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -9,12 +8,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = { nixpkgs, nixpkgs-old, home-manager, ... }: 
     let
       username = "dustin";
-      
-      # Define your systems with their architectures
+
       systems = {
         dsmac = "aarch64-darwin";
         dsstudio = "aarch64-darwin";
@@ -24,13 +21,37 @@
         pied = "aarch64-linux";
         thinky = "x86_64-linux";
       };
-      
-      # Generate all the homeConfigurations
+
+      # Bump duckdb past what's in nixpkgs (1.5.2 -> 1.5.4)
+      duckdbOverlay = final: prev: {
+        duckdb = prev.duckdb.overrideAttrs (old: {
+          version = "1.5.4";
+
+          src = final.fetchFromGitHub {
+            owner = "duckdb";
+            repo = "duckdb";
+            tag = "v1.5.4";
+            hash = "sha256-6xpKZKfH5/nwE2nU5kcpgITKFm3ilb1PYf9QEk+bKoM=";
+          };
+
+          cmakeFlags =
+            (final.lib.filter
+              (f: !(final.lib.hasInfix "OVERRIDE_GIT_DESCRIBE" f))
+              old.cmakeFlags)
+            ++ [
+              (final.lib.cmakeFeature "OVERRIDE_GIT_DESCRIBE"
+                "v1.5.4-0-g08e34c447bae34eaee3723cac61f2878b6bdf787")
+            ];
+
+          # doInstallCheck = false; # uncomment if the test suite chokes
+        });
+      };
+
       homeConfigurations = builtins.listToAttrs (
         builtins.map (hostname: 
           let 
             system = systems.${hostname};
-            pkgs = nixpkgs.legacyPackages.${system};
+            pkgs = (nixpkgs.legacyPackages.${system}).extend duckdbOverlay;
             pkgs-old = nixpkgs-old.legacyPackages.${system};
           in {
             name = "${username}@${hostname}";
