ea75c61f8a799d7ae59d07d7bc399bfd53e761ab
Dustin Sallings <dustin@spy.net>
2026-08-16 10:01:23 -1000

    Build a common darwin module

---
+++ home-manager/common/darwin.nix
@@ -0,0 +1,25 @@
+{ systemd, config, pkgs, lib, ... }:
+
+{
+  launchd.agents.pueue = {
+      enable = true;
+      config = {
+        Label = "net.spy.pueued";
+        ProgramArguments = [ "${pkgs.pueue}/bin/pueued" ];
+        RunAtLoad = true;
+  
+        # Relaunch on exit or crash
+        KeepAlive = true;
+  
+        StandardOutPath = "${config.xdg.stateHome}/my-server/stdout.log";
+        StandardErrorPath = "${config.xdg.stateHome}/my-server/stderr.log";
+  
+        EnvironmentVariables = {
+          PATH = lib.makeBinPath [ pkgs.pueue ];
+          HOME = config.home.homeDirectory;
+          LANG = "en_US.UTF-8";
+        };
+        WorkingDirectory = config.home.homeDirectory;
+      };
+    };
+}
--- home-manager/machines/dsstudio.nix
+++ home-manager/machines/dsstudio.nix
@@ -4,6 +4,7 @@
 {
   imports = [
     ../common/shared.nix
+    ../common/darwin.nix
   ];
 
   home = {
@@ -15,28 +16,6 @@
     # darcs
 	# pgcli
   ];
-
-  launchd.agents.pueue = {
-    enable = true;
-    config = {
-      Label = "net.spy.pueued";
-      ProgramArguments = [ "${pkgs.pueue}/bin/pueued" ];
-      RunAtLoad = true;
-
-      # Relaunch on exit or crash
-      KeepAlive = true;
-
-      StandardOutPath = "${config.xdg.stateHome}/my-server/stdout.log";
-      StandardErrorPath = "${config.xdg.stateHome}/my-server/stderr.log";
-
-      EnvironmentVariables = {
-        PATH = lib.makeBinPath [ pkgs.pueue ];
-        HOME = config.home.homeDirectory;
-        LANG = "en_US.UTF-8";
-      };
-      WorkingDirectory = config.home.homeDirectory;
-    };
-  };
 
   launchd.agents.duckupdate = {
     enable = true;
