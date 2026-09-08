058d6fe574c9784ea895dcf6094cf2937d662fbc
Dustin Sallings <dustin@spy.net>
2026-09-08 07:49:02 -1000

    fix log path for pueue

--- home-manager/common/darwin.nix
+++ home-manager/common/darwin.nix
@@ -19,13 +19,13 @@
         Label = "net.spy.pueued";
         ProgramArguments = [ "${pkgs.pueue}/bin/pueued" ];
         RunAtLoad = true;
-  
+
         # Relaunch on exit or crash
         KeepAlive = true;
-  
-        StandardOutPath = "${config.xdg.stateHome}/my-server/stdout.log";
-        StandardErrorPath = "${config.xdg.stateHome}/my-server/stderr.log";
-  
+
+        StandardOutPath = "${config.xdg.stateHome}/pueue/stdout.log";
+        StandardErrorPath = "${config.xdg.stateHome}/pueue/stderr.log";
+
         EnvironmentVariables = {
           PATH = lib.makeBinPath [ pkgs.pueue ];
           HOME = config.home.homeDirectory;
